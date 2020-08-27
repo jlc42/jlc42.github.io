92a15b9d85abd7f3e978aa1b8fd5e9d780d70a45
darren-stoll <darrenstoll11@gmail.com>
2020-08-27 15:35:34 -0600

    Updates

---
+++ precache-manifest.6457299f6dab3d539d75f7411f864db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f517d33de84fa97de95d1b98467021cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e513c1bef7298c70e35d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e69d68766598823b97b4",
+    "url": "/static/js/2.0307ce05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0307ce05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e513c1bef7298c70e35d",
+    "url": "/static/js/main.1db00b70.chunk.js"
+  },
+  {
+    "revision": "be75b41de9e8b140574a",
+    "url": "/static/js/runtime-main.c7efc78e.js"
+  }
+]);
