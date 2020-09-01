97bdea169080be16c96edbdd1fb9d3df4ded0eb0
darren-stoll <darrenstoll11@gmail.com>
2020-08-31 20:18:06 -0600

    Changed links in first section to modal links, will apply to rest after approval

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ import './App.css';
 // Honestly, I have no idea what the best way is to arrange all that, nor have I kept up with HTML and the various scripting tools people use these days (thus my call for help). 
 
 function App() {
+  // const [modal, setModal] = React.useState(false);
+
   let states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
 
   return (
     <div className="main">
-    
       <h1>JLC42 - COVID19 Data</h1>
       <a href="https://jlcarroll.blogspot.com/" rel="noopener noreferrer" target="_blank">Link to blog</a>
       <h2>Cases and Tests</h2>
@@ -35,6 +36,7 @@ function App() {
       <div id="rtLiveCodeFigs" className="rtLiveCodeFigs">
         <RTLiveCodeFigs />
       </div>
+      {/* <Modal show={modal} handleClose={() => setModal(!modal)} /> */}
     </div>
   );
 }
@@ -50,14 +52,16 @@ const CasesAndTestsByState = ({statesList}) => {
 
 // Component that returns an individual case and test data for that state
 const CaseAndTest = ({ location }) => {
+  const [modal, setModal] = React.useState(false);
   let caseLong = `https://raw.githubusercontent.com/jlc42/jlc42.github.io/master/figs/casesNTests/${location}-DailyCasesAndTests.png`;
   return (
-    <a href={caseLong} target="_blank" rel="noopener noreferrer">
-      <div className="case">
-        <img className="caseImage" src={caseLong} alt="" />
-        {location}
+    <>
+      <div className="case" onClick={() => {setModal(!modal)}}>
+          <img className="caseImage" src={caseLong} alt="" />
+          {location}
       </div>
-    </a>
+      <Modal show={modal} handleClose={() => setModal(!modal)} image={caseLong} location={location} type="Cases and Tests" content="Content" />
+    </>
   )
 }
 
@@ -124,6 +128,55 @@ const RTLiveCodeFigs = () => {
     </>
   )
 }
+
+// Modal component that handles the popup when clicking the image
+const Modal = ({ handleClose, show, image, location, type, content }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  // If "ESC" is pressed, it exits the modal window
+  const escFunction = (event) => {
+    if (event.keyCode === 27) { 
+      handleClose();
+    }
+  }
+
+  // If the mouse is clicked outside of the modal window while it is open, the modal will close
+  const handleClick = (event) => {
+    if (event.target.className === "modal display-block") {
+      handleClose();
+    }
+  }
+
+  // Side-effect that handles the ESC press or click out of modal window
+  React.useEffect(() => {
+    if (show) {
+      window.addEventListener('keydown', escFunction);
+      window.addEventListener('click', handleClick)
+      return (() => {
+        window.removeEventListener('keydown', escFunction);
+        window.removeEventListener('click', handleClick);
+      })
+    }
+  })
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        <span className="close" onClick={handleClose}>&times;</span>
+        <div className="modal-header">  
+          <h1>{location} - {type}</h1>
+        </div>
+        <img className="modal-image" src={image} alt={location} />
+        <div className="modal-content">
+        {content || "Lorem ipsum"}
+        </div>
+        <div className="modal-footer">
+          <button onClick={handleClose}>close</button>
+        </div>
+      </section>
+    </div>
+  )
+};
 
 
 export default App;
